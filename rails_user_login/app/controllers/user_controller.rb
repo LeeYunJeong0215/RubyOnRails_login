f2f0f9142d3d8e3568d9dc8b75f6659432498f21
LeeYunJeong0215 <yyj077643@gmail.com>
2017-12-21 14:13:57 +0900

    로그인유지메시지, 상태메시지출력

--- rails_user_login/app/controllers/user_controller.rb
+++ rails_user_login/app/controllers/user_controller.rb
@@ -28,18 +28,29 @@ class UserController < ApplicationController
     # 1. mail주소가 우리 db에 없으면(가입한 적이 없으면)
     if @user.nil?
       # 1-1. 없는 아이디라고 말해주고 -> 회원가입으로 유도
-      @msg ="존재하지 않는 회원입니다. 가입해주세요"
+      flash[:alert] = "존재하지 않는 아이디입니다. 등록해주세요."
+      redirect_to '/user/new'
+
     # 2. mail주소가 db에 있으면, 비밀번호를 확인
     else
       # 2-1. 맞으면, 로그인
       if @user.password == params[:password]
-          @msg ="로그인 성공"
           #로그인을 했는지 안했는지 유저가 계속 확인할 수 있도록
           session[:user_id] = @user.id
+          flash[:notice] = "#{@user.name}님 로그인에 성공하였습니다!"
+          redirect_to '/'
       # 2-2. 틀리면, 비밀번호가 틀렸습니다.
       else
-          @msg ="비밀번호를 확인해주세요"
+          flash[:alert] ="비밀번호가 틀렸습니다"
+          redirect_to :back
       end
     end
   end
+
+def logout
+  session.clear
+  flash[:notice] = "로그아웃에 성공하였습니다"
+  redirect_to '/'
+end
+
 end
--- rails_user_login/config/routes.rb
+++ rails_user_login/config/routes.rb
@@ -21,7 +21,9 @@ Rails.application.routes.draw do
 
 
   get 'user/login'
+  get 'user/logout'
   post 'user/login_process'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
